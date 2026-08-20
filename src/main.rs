cec65825ba4c37cde9d7f1a4d8382fc3b3d0e3e5
Lukiiy <lukiiy@proton.me>
2026-08-20 14:43:52 -0300

    basic server run call

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+use std::io::Result;
+use server::Server;
+use server::ServerConfig;
+
+fn main() -> Result<()> {
     println!("Hello, world!");
+
+    let config = ServerConfig::default();
+    let server = Server::new(config);
+
+    server.run()
 }
