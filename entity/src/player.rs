d0d42e657b01535187a3f6bb357b319124867569
Lukiiy <lukiiy@proton.me>
2026-08-30 18:56:09 -0300

    Player now stores position

--- entity/src/player.rs
+++ entity/src/player.rs
@@ -5,26 +5,39 @@ use std::io::{Result};
 use super::entity::Entity;
 use super::registry::{Identified, EntityRegistry};
 use protocol::PacketWriter;
+use world::Position;
 
 pub struct Player {
     pub entity: Entity,
     pub username: String,
 
+    position: Mutex<Position>,
     write: Mutex<TcpStream>
 }
 
 impl Player {
     pub fn new(entity: Entity, username: String, stream: TcpStream) -> Self {
+        let position = entity.position;
+
         Self {
             entity,
             username,
 
+            position: Mutex::new(position),
             write: Mutex::new(stream)
         }
     }
 
     pub fn id(&self) -> i32 {
         self.entity.id
+    }
+
+    pub fn position(&self) -> Position {
+        *self.position.lock().unwrap()
+    }
+
+    pub fn set_position(&self, position: Position) {
+        *self.position.lock().unwrap() = position;
     }
 
     pub fn send(&self, f: impl FnOnce(&mut PacketWriter<TcpStream>) -> Result<()>) -> Result<()> {
